e400abbb107b1fa07dc69b333567a4805e440088
Maurdekye <ncolaprete@gmail.com>
2022-10-22 22:26:28 -0400

    Added feature to auto-delete malformed epochs

--- scripts/train_epoch.py
+++ scripts/train_epoch.py
@@ -1,6 +1,7 @@
 from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 from argparse import ArgumentParser
 from pathlib import Path
+from shutil import rmtree
 import re
 import os
 from multiprocessing import cpu_count
@@ -25,12 +26,22 @@ parser.add_argument("--learn_rate", type=float, default=1e-6, help="learning rat
 parser.add_argument("--steps", type=int, default=1000, help="number of training steps to perform")
 parser.add_argument("--seed", type=int, default=3434554, help="training seed")
 parser.add_argument("--n_epochs", type=int, default=1, help="number of epochs to run and generate checkpoints for")
+parser.add_argument("--delete_malformed_models", action='store_true', help="Enable to allow the model to automatically delete the folders of malformed models that it detects")
 
-def execute(args, shell=False):
-    if shell:
-        args = " ".join(f'"{a}"' for a in args)
-    with Popen(args, stdout=PIPE, stderr=STDOUT, shell=shell) as p:
+def execute(args, env={}):
+    class BufferPrinter:
+        def __init__(self):
+            self.buf = ""
+        def print(self, msg, end=""):
+            print(msg, end=end)
+            self.buf += msg
+            self.buf = self.buf[-100000:]
+
+    environ = os.environ.copy()
+    environ.update(env)
+    with Popen(args, stdout=PIPE, stderr=STDOUT, env=environ) as p:
         buf = b""
+        rear_buffer = BufferPrinter()
         while not p.stdout.closed:
             chunk = p.stdout.read(8)
             if chunk == b'':
@@ -43,13 +54,16 @@ def execute(args, shell=False):
                     new_flush, buf = buf[:ind], buf[ind:]
                     flush += new_flush
             if flush != b"":
-                print(flush.decode("utf-8"), end='')
+                rear_buffer.print(flush.decode("utf-8"), end='')
                 flush = b""
-        print(buf.decode('utf-8'))
+        rear_buffer.print(buf.decode('utf-8'))
         
         returncode = p.poll()
         if returncode != 0:
-            raise CalledProcessError(returncode, p.args)
+            if all(err in rear_buffer.buf for err in ["EnvironmentError", "OSError: Can't load tokenizer"]):
+                raise ValueError()
+            else:
+                raise CalledProcessError(returncode, p.args)
 
 def print_arglist(args): print("\n".join(f"    {a}" for a in args))
 
@@ -61,94 +75,106 @@ def main(args):
             raise ValueError("Please provide model name via --model_name if no train path is specified")
 
     os.makedirs(str(args.train_path.resolve()), exist_ok=True)
-    if not any(path for path in args.train_path.iterdir() if path.is_dir() and epoch_regex.match(path.name) != None):
-        if args.base_checkpoint != None:
-            # no existing epoch exists, create epoch_0 from the provided checkpoint
-            base_epoch_path = args.train_path / 'epoch_0'
-            create_base_epoch_args = [
-                "python", relpath("convert_original_stable_diffusion_to_diffusers.py"),
-                f"--checkpoint_path={args.base_checkpoint}",
-                f"--dump_path={base_epoch_path}"
-            ]
-            print("Creating base model with these args:")
-            print_arglist(create_base_epoch_args)
-            try:
-                execute(create_base_epoch_args)
-            except CalledProcessError:
-                pass
-            if not base_epoch_path.is_dir():
-                print("failed to create base epoch, exiting")
-                return
-        else:
-            raise Exception("No existing epochs found, please provide a base model file to train from.")
 
     for epoch in range(args.n_epochs):
-        # train the epoch
-        existing_epochs = sorted(((p, e) for p, e in ((path, epoch_regex.match(path.name)) for path in args.train_path.iterdir() if path.is_dir()) if e != None), key=(lambda p: int(p[1].group(1))), reverse=True)
-        _, latest_epoch_regex = existing_epochs[0]
-        old_epoch_num = int(latest_epoch_regex.group(1))
-        new_epoch_num = old_epoch_num + 1
-        old_epoch_path = args.train_path / f"epoch_{old_epoch_num}"
-        new_epoch_path = args.train_path / f"epoch_{new_epoch_num}"
-        print(f"Beginning training on epoch {new_epoch_num}" + ('' if args.n_epochs == 1 else f", {epoch+1}/{args.n_epochs}"))
+        while True:
+            # if no existing epoch exists, create epoch_0 from the provided checkpoint
+            if not any(path for path in args.train_path.iterdir() if path.is_dir() and epoch_regex.match(path.name) != None):
+                if args.base_checkpoint != None:
+                    base_epoch_path = args.train_path / 'epoch_0'
+                    create_base_epoch_args = [
+                        "python", relpath("convert_original_stable_diffusion_to_diffusers.py"),
+                        f"--checkpoint_path={args.base_checkpoint}",
+                        f"--dump_path={base_epoch_path}"
+                    ]
+                    print("Creating base model with these args:")
+                    print_arglist(create_base_epoch_args)
+                    try:
+                        execute(create_base_epoch_args)
+                    except CalledProcessError:
+                        pass
+                    if not base_epoch_path.is_dir():
+                        print("failed to create base epoch, exiting")
+                        return
+                else:
+                    raise Exception("No existing epochs found, please provide a base model file to train from.")
 
-        train_args = [
-            "accelerate", "launch",
-            f"--num_cpu_threads_per_process={cpu_count()}",
-            relpath("../examples/dreambooth/train_dreambooth.py"),
-            f"--pretrained_model_name_or_path={old_epoch_path}",
-            f"--instance_data_dir={args.source_path}",
-            f"--output_dir={new_epoch_path}",
-            f"--instance_prompt={args.instance_prompt}",
-            f"--seed={args.seed}",
-            "--resolution=512",
-            "--train_batch_size=1",
-            "--train_text_encoder",
-            "--mixed_precision=fp16",
-            "--use_8bit_adam",
-            "--gradient_accumulation_steps=1",
-            f"--learning_rate={args.learn_rate}",
-            "--lr_scheduler=constant",
-            "--lr_warmup_steps=0",
-            "--sample_batch_size=4",
-            f"--max_train_steps={args.steps}"
-        ]
-        if None not in [args.class_prompt, args.class_path]:
-            train_args.extend([
-                f"--class_prompt={args.class_prompt}",
-                "--with_prior_preservation", 
-                "--prior_loss_weight=1.0",
-                f"--class_data_dir={args.class_path}",
-                f"--num_class_images={sum(1 for p in args.class_path.iterdir() if p.is_file())}"
-            ])
-        
-        print("training with these args:")
-        print_arglist(train_args)
+            # train the epoch
+            existing_epochs = sorted(((p, e) for p, e in ((path, epoch_regex.match(path.name)) for path in args.train_path.iterdir() if path.is_dir()) if e != None), key=(lambda p: int(p[1].group(1))), reverse=True)
+            _, latest_epoch_regex = existing_epochs[0]
+            old_epoch_num = int(latest_epoch_regex.group(1))
+            new_epoch_num = old_epoch_num + 1
+            old_epoch_path = args.train_path / f"epoch_{old_epoch_num}"
+            new_epoch_path = args.train_path / f"epoch_{new_epoch_num}"
+            print(f"Beginning training on epoch {new_epoch_num}" + ('' if args.n_epochs == 1 else f", {epoch+1}/{args.n_epochs}"))
 
-        try:
-            os.environ["LD_LIBRARY_PATH"] = f"/usr/lib/wsl/lib"
-            execute(train_args)
-        except CalledProcessError:
-            print("Error during training, skipping model export")
-            continue
-
-        # export the epoch
-        if None not in [args.export_path, args.model_name]:
-            full_export_path = args.export_path / f"{args.model_name}_e{new_epoch_num}.ckpt"
-            export_args = [
-                "python",
-                relpath("convert_diffusers_to_original_stable_diffusion.py"),
-                f"--model_path={new_epoch_path}",
-                f"--checkpoint_path={full_export_path}"
+            train_args = [
+                "accelerate", "launch",
+                f"--num_cpu_threads_per_process={cpu_count()}",
+                relpath("../examples/dreambooth/train_dreambooth.py"),
+                f"--pretrained_model_name_or_path={old_epoch_path}",
+                f"--instance_data_dir={args.source_path}",
+                f"--output_dir={new_epoch_path}",
+                f"--instance_prompt={args.instance_prompt}",
+                f"--seed={args.seed}",
+                "--resolution=512",
+                "--train_batch_size=1",
+                "--train_text_encoder",
+                "--mixed_precision=fp16",
+                "--use_8bit_adam",
+                "--gradient_accumulation_steps=1",
+                f"--learning_rate={args.learn_rate}",
+                "--lr_scheduler=constant",
+                "--lr_warmup_steps=0",
+                "--sample_batch_size=4",
+                f"--max_train_steps={args.steps}"
             ]
-            print(f"exporting model with these args:")
-            print_arglist(export_args)
+            if None not in [args.class_prompt, args.class_path]:
+                train_args.extend([
+                    f"--class_prompt={args.class_prompt}",
+                    "--with_prior_preservation", 
+                    "--prior_loss_weight=1.0",
+                    f"--class_data_dir={args.class_path}",
+                    f"--num_class_images={sum(1 for p in args.class_path.iterdir() if p.is_file())}"
+                ])
+            
+            print("training with these args:")
+            print_arglist(train_args)
 
             try:
-                execute(export_args)
+                execute(train_args, env={"LD_LIBRARY_PATH": "/usr/lib/wsl/lib"})
+            except ValueError:
+                print("Detected a malformed model folder from a previously crashed or cancelled training session.")
+                rmtree(new_epoch_path)
+                if args.delete_malformed_models:
+                    print(f"Deleting model folder {old_epoch_path}")
+                    rmtree(old_epoch_path)
+                    continue
+                else:
+                    print("If you would like the script to automatically delete this model folder and continue training, pass --delete_malformed_models")
+                    return
             except CalledProcessError:
-                print("Error during model export")
-                continue
+                print("Error during training, skipping model export")
+                break
+
+            # export the epoch
+            if None not in [args.export_path, args.model_name]:
+                full_export_path = args.export_path / f"{args.model_name}_e{new_epoch_num}.ckpt"
+                export_args = [
+                    "python",
+                    relpath("convert_diffusers_to_original_stable_diffusion.py"),
+                    f"--model_path={new_epoch_path}",
+                    f"--checkpoint_path={full_export_path}"
+                ]
+                print(f"exporting model with these args:")
+                print_arglist(export_args)
+
+                try:
+                    execute(export_args)
+                except CalledProcessError:
+                    print("Error during model export")
+
+            break
     
     print("Finished training!")
     
